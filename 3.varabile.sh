d979c8b31d63bd39626051c466eff09948aac6eb
poojithkumar29 <poojithkumar.p123@gmail.com>
2026-02-15 07:34:14 +0530

    variables

--- 3.varabile.sh
+++ 3.varabile.sh
@@ -3,7 +3,7 @@
 PERSON1=poojith
 PERSON2=chandu
 
-echo "PERSON1:Hi this is PERSON1"
-echo "PERSON2::Hi this PERSON2"
-echo "PERSON1::what about your carrier"
-echo "PERSON2::Learing devops and What about you PERSON1"
+echo "$PERSON1:Hi this is $PERSON1"
+echo "$PERSON2::Hi this $PERSON2"
+echo "$PERSON1::what about your carrier"
+echo "$PERSON2::Learing devops and What about you $PERSON1"
