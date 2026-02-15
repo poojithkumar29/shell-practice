ed3c720c92fa00bab4dd81832bafbc20e58977dc
poojithkumar29 <poojithkumar.p123@gmail.com>
2026-02-15 07:50:45 +0530

    some message

--- 3.varabile.sh
+++ 3.varabile.sh
@@ -6,4 +6,4 @@ PERSON2=chandu
 echo "$PERSON1:Hi this is $PERSON1"
 echo "$PERSON2::Hi this $PERSON2"
 echo "$PERSON1::what about your carrier"
-echo "$PERSON2::Learing devops and What about you $PERSON1"
+echo "$PERSON2::Learing devops and What about you\ $PERSON1"
