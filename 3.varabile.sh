d8e8ceb4382cc7a106c8b9ebfe36914b720dd619
poojithkumar29 <poojithkumar.p123@gmail.com>
2026-02-15 07:32:47 +0530

    variables

---
+++ 3.varabile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=poojith
+PERSON2=chandu
+
+echo "PERSON1:Hi this is PERSON1"
+echo "PERSON2::Hi this PERSON2"
+echo "PERSON1::what about your carrier"
+echo "PERSON2::Learing devops and What about you PERSON1"
