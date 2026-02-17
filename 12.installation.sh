64e3a2689639846d41a2f59303cb0793c9d2c9f7
poojithkumar29 <poojithkumar.p123@gmail.com>
2026-02-17 15:03:40 +0530

    Installation

---
+++ 12.installation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]; then
+    echo "ERROR :: Please use this script in Root privelage"
+    exit 1
+fi 
+
+validate(){
+    if [ $1 -ne 0]; then
+        echo "ERROR :: Installation of $2 is Failure"
+        exit 1
+    else
+        echo "Installation of $2 is Succesfull"
+
+}
+dnf install mysql -y
+validate $? "Mysql"
+
+dnf install nginx -y
+validate $? "Nginx"
