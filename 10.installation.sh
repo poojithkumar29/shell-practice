69504fc79bee3f920d836f07fed221007afa655a
poojithkumar29 <poojithkumar.p123@gmail.com>
2026-02-17 14:17:57 +0530

    Installation

--- 10.installation.sh
+++ 10.installation.sh
@@ -3,7 +3,7 @@
 
 USERID=$(id -u)
 
-if if [ $USERID -ne 0]; then
+if  [ $USERID -ne 0]; then
     echo "ERROR::Please use this script in root privellage "
     exit 1
 fi 
@@ -12,7 +12,7 @@ dnf install mysql -y
  
 if [ $? -ne 0]; then    
     echo "The MYSQl is not installed succesfully"
-    exit1
+    exit 1
 else 
     echo "MYSQL is succesfully Installed"
 fi
