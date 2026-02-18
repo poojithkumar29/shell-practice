3803a2ec4a8f6463dd7ec4caad25f645b8fba609
poojithkumar29 <poojithkumar.p123@gmail.com>
2026-02-18 23:22:04 +0530

    Logs

--- 15-logs.sh
+++ 15-logs.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
 LOGS_FOLDER="/var/log/shell-script"
 Script_name=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$Script_name.log"
 mkdir -p $LOGS_FOLDER
     echo "Script are started executed at: $(date)" | tee -a $LOG_FILE
+
+if [$USERID -ne 0]; then 
+ echo "ERROR::please run this script in ROOT privelege"
+ exit1
+fi
