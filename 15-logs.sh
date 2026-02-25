44884effad529a9f96e6ddec970e52b8beee7dbd
poojithkumar29 <poojithkumar.p123@gmail.com>
2026-02-25 21:43:50 +0530

    Logs

--- 15-logs.sh
+++ 15-logs.sh
@@ -9,15 +9,15 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-script"
-Script_name= $(echo $0 | cut -d "." -f1)
+Script_name= $( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$Script_name.log"
 
 mkdir -p $LOGS_FOLDER
 echo "Script executed time :: $(date)"
 
-if [ $USERID -ne 0 ]: then
+if [ $USERID -ne 0 ]; then
    echo "ERROR:: Please enter the scripts in ROOT privelage"
-   exit1
+   exit 1
 fi
 
 validate()
@@ -25,7 +25,7 @@ if [ $1 -ne 0 ]; then
     echo "Installation.............Failure"
 else
     echo "INstallation.............Sucessfull"
-    exit
+    exit 1
 fi
 
 dnf list installed mysql
