c3a9ab0af79b456f47d5d8c9016039518981c99d
poojithkumar29 <poojithkumar.p123@gmail.com>
2026-02-25 21:41:15 +0530

    Logs

--- 15-logs.sh
+++ 15-logs.sh
@@ -2,13 +2,36 @@
 
 USERID=$(id -u)
 
-LOGS_FOLDER="/var/log/shell-script"
-Script_name=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$Script_name.log"
-mkdir -p $LOGS_FOLDER
-    echo "Script are started executed at: $(date)" | tee -a $LOG_FILE
 
-if [ $USERID -ne 0 ]; then 
- echo "ERROR::please run this script in ROOT privelege"
- exit1
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+Script_name= $(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$Script_name.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script executed time :: $(date)"
+
+if [ $USERID -ne 0 ]: then
+   echo "ERROR:: Please enter the scripts in ROOT privelage"
+   exit1
+fi
+
+validate()
+if [ $1 -ne 0 ]; then
+    echo "Installation.............Failure"
+else
+    echo "INstallation.............Sucessfull"
+    exit
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+  dnf install mysql-server -y
+  validate $? "MYSQL"
+else 
+    echo "Already MYSQL is exsist.........skipping"
 fi
